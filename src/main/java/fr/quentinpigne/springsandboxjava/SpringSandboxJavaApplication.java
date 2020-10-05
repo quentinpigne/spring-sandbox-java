96f73e25fdfdd41bd1c34233b8cb01193bcbab5f
Quentin Pigné <quentin.pigne@gmail.com>
2020-10-05 11:24:59 +0200

    Initial commit

---
+++ src/main/java/fr/quentinpigne/springsandboxjava/SpringSandboxJavaApplication.java
@@ -0,0 +1,13 @@
+package fr.quentinpigne.springsandboxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandboxJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSandboxJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/quentinpigne/springsandboxjava/SpringSandboxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.quentinpigne.springsandboxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSandboxJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
