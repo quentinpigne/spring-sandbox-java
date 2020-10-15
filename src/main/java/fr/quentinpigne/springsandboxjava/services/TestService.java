d4bde8c68b645e5ec071a9ecb7e0023718d5a380
Quentin Pigné <quentin.pigne@gmail.com>
2020-10-15 16:16:39 +0200

    enabling cache

--- src/main/java/fr/quentinpigne/springsandboxjava/services/TestService.java
+++ src/main/java/fr/quentinpigne/springsandboxjava/services/TestService.java
@@ -1,12 +1,14 @@
 package fr.quentinpigne.springsandboxjava.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TestService {
 
+    @Cacheable("longRunningTreatment")
     public Integer longRunningTreatment() throws InterruptedException {
         Thread.sleep(1000);
         return 42;
