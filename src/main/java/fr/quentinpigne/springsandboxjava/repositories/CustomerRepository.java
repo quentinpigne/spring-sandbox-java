c26500383f22244d85adcc821ebe6c0068426ed0
Quentin Pigné <quentin.pigne@gmail.com>
2020-12-04 18:51:26 +0100

    using @RepositoryRestResource annotation

--- src/main/java/fr/quentinpigne/springsandboxjava/repositories/CustomerRepository.java
+++ src/main/java/fr/quentinpigne/springsandboxjava/repositories/CustomerRepository.java
@@ -2,12 +2,12 @@ package fr.quentinpigne.springsandboxjava.repositories;
 
 import fr.quentinpigne.springsandboxjava.entities.Customer;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+@RepositoryRestResource
 public interface CustomerRepository extends CrudRepository<Customer, UUID> {
 
     List<Customer> findByCode(Long code);
